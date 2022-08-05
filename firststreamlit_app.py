337ac1cd4739da370e6e286635e84fa58d881499
poojatechwork <110658829+poojatechwork@users.noreply.github.com>
2022-08-05 13:03:14 +0530

    Update firststreamlit_app.py
    added few headers

--- firststreamlit_app.py
+++ firststreamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
-streamlit.title("My Parents new diner")
-
-streamlit.header("Breakfast menu")
-
-streamlit.text("Kale juice")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
